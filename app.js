62d2cc1b9e5f086fbf5c5c41063ca42189803d7a
technicalxakash <akashr2468@gmail.com>
2026-01-05 15:02:13 +0530

    1day datatype conversion

--- app.js
+++ app.js
@@ -19,6 +19,52 @@
 // b="ro"
 // console.log(b)
 
-const a="akash"
-a="nitin"
-console.log(a)
+// const a="akash"
+// a="nitin"
+// console.log(a)
+
+//1)primitive dataypes
+// String
+// Number
+// Boolean
+// Null
+// undefined
+// Symbol
+
+// 2)refernecd Types
+// arrays
+// objectliterals
+// Functions
+
+// const a="akash"
+// const b=90;
+// console.log(typeof a)
+// console.log(typeof b)
+
+//refrence dataTypes 
+// arrays
+// const hobbies=["may","june"]
+// console.log(typeof hobbies)
+
+// // objectliterals
+
+// const a={
+
+//         month:"may",
+//         year:2003
+// };
+// console.log(typeof a)
+
+// // dates
+
+// const today=new Date();
+
+// console.log(today)
+// console.log(typeof today)
+
+
+//DataTypes Conversion
+
+ const val=String(5+5)
+console.log(typeof val)
+console.log(val.length)
