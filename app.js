5f8e495f5334a6c960e973d1e288f75ecae14028
technicalxakash <akashr2468@gmail.com>
2026-01-05 13:04:35 +0530

    1day var

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 //alert("hello");
-console.log(123);
-console.log("microAkash")
-var a="hello";
-console.log(a);
-console.log([1,2,3,4])
-console.log({a:1,b:2,c:3})
+// console.log(123);
+// console.log("microAkash")
+// var a="hello";
+// console.log(a);
+// console.log([1,2,3,4])
+// console.log({a:1,b:2,c:3})
+
+//variables
+//var,let,const
+//var is not used in modern js it is deprecated
+//let and const should be used 
+
+// var a="akash";
+// console.log(a)
+// a="sai"
+// console.log(a)
+// var b="beve"
+// b="ro"
+// console.log(b)
+
+const a="akash"
+a="nitin"
+console.log(a)
