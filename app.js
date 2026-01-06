26d7e09a0b4c580ab8bab79e471306e99362735f
technicalxakash <akashr2468@gmail.com>
2026-01-06 12:06:47 +0530

    DOM

--- app.js
+++ app.js
@@ -127,3 +127,18 @@
 
 //function Expressions
 
+/////////////////DOM
+
+let val;
+val=document;
+val=document.all;
+val=document.all[0];
+val=document.all.length;
+val=document.head;
+val=document.body;
+val=document.doctype;
+val=document.domain;
+val=document.URL; 
+console.log(val)
+
+ 
